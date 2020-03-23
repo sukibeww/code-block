d56c1ff73c345c0c25cf7fead65fd191a448fb9c
Sukianto Suteja <sukialiong@gmail.com>
2020-03-23 17:24:49 +1100

    added zdog on index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Code:Block`,
-    introduction: `Hi! My name is Suki, a web developer based in Melbourne, Australia. The purpose of this blog is just to document my journey as a developer and interests. All of my post will be my opinion or insight, so... take everything I wrote with a grain of salt.`,
-    description: `This is my personal blog, This blog will be about all of the things that I find interesting and serve as my creative outlet. I hope this blog can be educational or provide some sort of insight.`,
+    introduction: `Hi! My name is Suki, a web developer based in West Jakarta, Indonesia. The purpose of this blog is just to document my journey as a developer and interests. All of my post will be my opinion or insight, so... take everything I wrote with a grain of salt.`,
+    description: `This is my personal blog, This blog will be about all of the things that I find interesting and serve as my creative outlet. I hope this blog can be somewhat educational and helpful.`,
     author: `suki_aliong`,
     siteUrl: `https://cd-block.netlify.com`
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,41 @@
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import React from "react"
+import React , { useState, useEffect, useRef }from "react"
 import { AiOutlineBlock } from "react-icons/ai"
 import styled from 'styled-components'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {
+  Illustration,
+  Shape,
+  Rect,
+  Box,
+  useRender,
+} from 'react-zdog'
+import { a, useSpring } from '@react-spring/zdog'
+
+const TAU = Math.PI * 2
+const Ring = (props) => {
+  const [up, setUp] = useState(true)
+  useEffect(() => void setInterval(() => setUp(previous => !previous), 450), [])
+  const { rotation } = useSpring({
+    rotation: Math.PI,
+  })
+  const ref = useRef()
+  let t = 0
+  useRender(() => {
+    ref.current.rotate.x += props.speed;
+    ref.current.rotate.y += props.speed;
+  })
+  return (
+    <Shape ref={ref} stroke={0} color="#747B9E">
+      <a.Anchor rotate={rotation.interpolate(r => ({ x: TAU / 18 + -r / 4 }))}>
+        <Rect width={50} height={50} stroke={1} color="#A4A4A4"/>
+      </a.Anchor>
+    </Shape>
+  )
+}
+
 
 const Subheader = styled.h3`
   color: #373737;
@@ -22,7 +53,6 @@ const MainHeader = styled.h1`
   justify-content: center;
   align-items: center;
 `
-
 
 const IntroPaper = styled.div`
   background-color: #FFFFFF;
@@ -60,8 +90,25 @@ export default ({data}) => {
         <IntroContainer>
           <IntroPaper>
             <MainHeader>Welcome to {data.site.siteMetadata.title} <AiOutlineBlock/> </MainHeader>
-            <div style={{width: "80%"}}>
-              <Img fluid={data.file.childImageSharp.fluid}  alt="landing" />
+            <div style={{width: "300px", height: "300px"}}>
+              <Illustration dragRotate={true} zoom={3}>
+                <Ring speed={0.03} diameter={80}/>
+                <Ring speed={0.04} diameter={70}/>
+                <Ring speed={0.07} diameter={60}/>
+                <Box 
+                rotate={{y: 10, x: 10}}
+                stroke={false}  
+                width={5} 
+                height={5} 
+                depth={5} 
+                color="#3F3F3F" 
+                topFace="#5e5e5e" 
+                rightFace="#494949" 
+                leftFace="#636363" 
+                frontFace="#9E9E9E" 
+                bottomFace="#7A7A7A"
+                /> 
+              </Illustration>
             </div>
             <Subheader>{data.site.siteMetadata.introduction}</Subheader>
             <Subheader>{data.site.siteMetadata.description}</Subheader>
